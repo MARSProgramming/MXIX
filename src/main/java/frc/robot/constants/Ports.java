1feffa14ff52c6fe41a0904c7db48c439f020ed3
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2026-01-29 11:45:44 -0500

    added initial configs
    untuned

--- src/main/java/frc/robot/constants/Ports.java
+++ src/main/java/frc/robot/constants/Ports.java
@@ -1,5 +1,57 @@
 package frc.robot.constants;
 
 public class Ports {
-    
+    public static class Limelight {
+        public static final String kShooterLimelightName = "llshooter";
+        public static final String kBackLimelightName = "llback";
+    }
+
+    public static class Drivetrain {
+        public static final int kFrontRightDrive = 1;
+        public static final int kFrontRightSteer = 2;
+        
+        public static final int kFrontLeftDrive = 7;
+        public static final int kFrontLeftSteer = 8;
+
+        public static final int kBackLeftDrive = 5;
+        public static final int kBackLeftSteer = 6;
+
+        public static final int kBackRightDrive = 3;
+        public static final int kBackRightSteer = 4;
+    }
+
+    public static class Flywheel {
+        public static final int kLeftFlywheelMaster = 9;
+        public static final int kLeftFlywheelFollower = 10;
+
+        public static final int kRightFlywheelMaster = 11;
+        public static final int kRightFlywheelFollower = 12;
+    }
+
+    public static class Cowl {
+        public static final int kCowlMotor = 13;
+    }
+
+    public static class Intake {
+        public static final int kIntakePivot = 14;
+        public static final int kIntakeRollers = 15;
+    }
+
+    public static class Floor {
+        public static final int kFloorRollers = 16;
+    }
+
+    public static class FlipClimber {
+        public static final int kTopClimber = 17;
+        public static final int kBottomClimber = 18;
+        public static final int kLinearActuator = 0; // TODO: Find out
+    }
+
+    public static class FastClimber {
+        public static final int kHookClimber = 19;
+    }
+
+    public static class Feeder {
+        public static final int kFeederMotor = 20;
+    }
 }
--- src/main/java/frc/robot/constants/SystemConstants.java
+++ src/main/java/frc/robot/constants/SystemConstants.java
@@ -1,31 +1,16 @@
 package frc.robot.constants;
 
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.LinearVelocity;
 import frc.robot.generated.TunerConstants;
 
 public class SystemConstants {
-    
-    public static class AutoAim {
-
-        // SOTM
-        public static final double AUTO_AIM_MAX_DRIVETRAIN_VEL = 6.4; 
-        // Compensation fudge factor for rotation
-        public static final double ROTATION_VELOCITY_COMPENSATION_FACTOR = 0.1;
-
-        //Turn to pose constants
-        public static final double AUTO_AIM_MARGIN = 1.5;
-        public static final double AUTO_AIM_SETPOINT_MARGIN = 5; // Degrees
-        public static final double AUTO_AIM_MAX_VEL_SETPOINT = 1.5; // The maximum commanded rotational velocity in rad/s
-        public static final double AUTO_AIM_MAX_ROT_VEL = 4.4;
-        public static final double AUTO_AIM_KP = 0.1;
-        public static final double AUTO_AIM_KD = 0; 
-
-    }
-
-
     public static class Drive {
         public static final LinearVelocity kMaxSpeed = TunerConstants.kSpeedAt12Volts;
         public static final AngularVelocity kMaxRotationalRate = Units.RotationsPerSecond.of(1);
@@ -34,5 +19,206 @@ public class SystemConstants {
 
     public static class KrakenX60 {
         public static final AngularVelocity kFreeSpeed = Units.RPM.of(6000);
+    }
+
+    public static class KrakenX44 {
+        public static final AngularVelocity kFreeSpeed = Units.RPM.of(7530);
+    }
+
+    public static class Flywheel {
+        public static final double kFlywheelReduction = 18.0 / 17.0;
+        private static final AngularVelocity kMaxFlywheelSpeed = KrakenX60.kFreeSpeed.div(kFlywheelReduction);
+        
+        public static TalonFXConfiguration masterConfig = new TalonFXConfiguration();        
+        static {
+            masterConfig.Slot0.kP = 0.5;
+            masterConfig.Slot0.kI = 2;
+            masterConfig.Slot0.kD = 0;
+            masterConfig.Slot0.kV = 12.0 / kMaxFlywheelSpeed.in(Units.RotationsPerSecond);
+
+            masterConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+            masterConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+            masterConfig.Feedback.SensorToMechanismRatio = kFlywheelReduction; 
+
+            masterConfig.Voltage.PeakReverseVoltage = 0; // Software lock reversal of flywheel 
+            masterConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            masterConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            masterConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            masterConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+
+    public static class Cowl {
+        public static final double kCowlReduction = 24.0 / 18.0;
+        private static final AngularVelocity kMaxCowlSpeed = KrakenX44.kFreeSpeed.div(kCowlReduction);
+        public static final double kCowlStallCurrent = 1; // Configure with testing, this is way too low
+
+        public static TalonFXConfiguration cowlConfig = new TalonFXConfiguration();
+        static {
+            // Position Control pid (requires tuning)
+            cowlConfig.Slot0.kP = 1;
+            cowlConfig.Slot0.kI = 0;
+            cowlConfig.Slot0.kD = 0;
+            cowlConfig.Slot0.kV = 12.0 / kMaxCowlSpeed.in(Units.RotationsPerSecond);
+
+            cowlConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+            cowlConfig.Feedback.SensorToMechanismRatio = kCowlReduction; // Gear ratio
+
+            cowlConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            cowlConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            cowlConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            cowlConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+    
+    public static class Intake {
+        public static final double kIntakeReduction = 18.0 / 17.0; // Same for both pivot and rollers
+        private static final AngularVelocity kMaxIntakeSpeed = KrakenX44.kFreeSpeed.div(kIntakeReduction); // same for both pivot and rollers
+        public static final double kIntakePivotStallCurrent = 1; // Configure with testing, this is way too low
+
+        public static TalonFXConfiguration pivotConfig = new TalonFXConfiguration();
+        public static TalonFXConfiguration rollerConfig = new TalonFXConfiguration();
+
+        static {
+            // Position Control pid (requires tuning)
+            pivotConfig.Slot0.kP = 1;
+            pivotConfig.Slot0.kI = 0;
+            pivotConfig.Slot0.kD = 0;
+            pivotConfig.Slot0.kV = 12.0 / kMaxIntakeSpeed.in(Units.RotationsPerSecond);
+
+            pivotConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+            pivotConfig.Feedback.SensorToMechanismRatio = kIntakeReduction; // Gear ratio
+
+            pivotConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            pivotConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            pivotConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            pivotConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+
+        static {
+            // Velocity control PID (if needed)
+            rollerConfig.Slot0.kP = 0.5;
+            rollerConfig.Slot0.kI = 2;
+            rollerConfig.Slot0.kD = 0;
+            rollerConfig.Slot0.kV = 12.0 / kMaxIntakeSpeed.in(Units.RotationsPerSecond);
+
+            rollerConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+            rollerConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+            rollerConfig.Feedback.SensorToMechanismRatio = kIntakeReduction; 
+
+            rollerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            rollerConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            rollerConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            rollerConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+
+    public static class Floor {
+        public static final double kFloorReduction = 1; // TODO: Find out
+        private static final AngularVelocity kMaxFloorSpeed = KrakenX44.kFreeSpeed.div(kFloorReduction);
+        
+        public static TalonFXConfiguration floorConfig = new TalonFXConfiguration();
+
+        static {
+            // Velocity control (if needed)
+            floorConfig.Slot0.kP = 0.5;
+            floorConfig.Slot0.kI = 2;
+            floorConfig.Slot0.kD = 0;
+            floorConfig.Slot0.kV = 12.0 / kMaxFloorSpeed.in(Units.RotationsPerSecond);
+
+            floorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast; // TODO: Should this be in brake mode? Should the feeder be either?
+            floorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: Find out
+
+            floorConfig.Feedback.SensorToMechanismRatio = kFloorReduction; 
+
+            floorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            floorConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            floorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            floorConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+
+    
+    public static class Feeder {
+        public static final double kFloorReduction = 1; // TODO: Find out
+        private static final AngularVelocity kMaxFeederSpeed = KrakenX60.kFreeSpeed.div(kFloorReduction);
+        
+        public static TalonFXConfiguration feederConfig = new TalonFXConfiguration();
+
+        static {
+            // Velocity control (if needed)
+            feederConfig.Slot0.kP = 1;
+            feederConfig.Slot0.kI = 0;
+            feederConfig.Slot0.kD = 0;
+            feederConfig.Slot0.kV = 12.0 / kMaxFeederSpeed.in(Units.RotationsPerSecond);
+
+            feederConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+            feederConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: Find out
+
+            feederConfig.Feedback.SensorToMechanismRatio = kFloorReduction; 
+
+            feederConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            feederConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            feederConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            feederConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+
+    
+    
+    public static class FastClimber {
+        public static final double kFastClimberReduction = 80 / 1; // TODO: Find out
+        private static final AngularVelocity kMaxFastClimberSpeed = KrakenX60.kFreeSpeed.div(kFastClimberReduction);
+        
+        public static TalonFXConfiguration fastClimberConfig = new TalonFXConfiguration();
+
+        static {
+            // position control (if needed)
+            fastClimberConfig.Slot0.kP = 1; // definitely too low if we need position control
+            fastClimberConfig.Slot0.kI = 0;
+            fastClimberConfig.Slot0.kD = 0;
+            fastClimberConfig.Slot0.kV = 12.0 / kMaxFastClimberSpeed.in(Units.RotationsPerSecond);
+
+            fastClimberConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            fastClimberConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: Find out
+
+            fastClimberConfig.Feedback.SensorToMechanismRatio = kFastClimberReduction; 
+
+            fastClimberConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            fastClimberConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            fastClimberConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            fastClimberConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
+    }
+
+    
+    
+    public static class FlipClimber {
+        public static final double kFlipClimberReduction = 80 / 1; // TODO: Find out
+        private static final AngularVelocity kMaxFastClimberSpeed = KrakenX60.kFreeSpeed.div(kFlipClimberReduction);
+        
+        public static TalonFXConfiguration fastClimberConfig = new TalonFXConfiguration();
+
+        static {
+            // position control (if needed)
+            fastClimberConfig.Slot0.kP = 1; // definitely too low if we need position control
+            fastClimberConfig.Slot0.kI = 0;
+            fastClimberConfig.Slot0.kD = 0;
+            fastClimberConfig.Slot0.kV = 12.0 / kMaxFastClimberSpeed.in(Units.RotationsPerSecond);
+
+            fastClimberConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            fastClimberConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: Find out
+
+            fastClimberConfig.Feedback.SensorToMechanismRatio = kFlipClimberReduction; 
+
+            fastClimberConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            fastClimberConfig.CurrentLimits.SupplyCurrentLimit = 70;
+            fastClimberConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            fastClimberConfig.CurrentLimits.StatorCurrentLimit = 120;
+        }
     }
 }
