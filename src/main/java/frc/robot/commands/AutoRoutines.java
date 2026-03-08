1928d4aaf09ebdfd9b854d4dc97c919d6271c757
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2026-03-08 18:00:07 -0400

    added auto routines

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -4,6 +4,12 @@
 
 package frc.robot.commands;
 
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineNoSweep$0;
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineNoSweep$1;
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineNoSweep$2;
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineSweep$0;
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineSweep$1;
+import static frc.robot.util.ChoreoTraj.LeftCenterBeelineSweep$2;
 import static frc.robot.util.ChoreoTraj.OutpostTrajectory$0;
 import static frc.robot.util.ChoreoTraj.OutpostTrajectory$1;
 import static frc.robot.util.ChoreoTraj.OutpostTrajectory$2;
@@ -17,6 +23,7 @@ import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.robot.constants.Settings;
 import frc.robot.subsystems.Cowl;
 import frc.robot.subsystems.FastClimber;
 import frc.robot.subsystems.Feeder;
@@ -82,56 +89,15 @@ public final class AutoRoutines {
      */
     public void configure() {
         autoChooser.addRoutine("driveToPreloaded pos", this::drivetoPreloadShootPos);
-        autoChooser.addRoutine("reset Pose", this::resetPoseAtTrench);
+        autoChooser.addRoutine("Left Side Center Beeline", this::leftBumpBeelineToCenter);
+        autoChooser.addRoutine("Left Side Center Beeline Sweep", this::leftBumpBeelineAndSweepCenter);
+        
         SmartDashboard.putData("Auto Chooser", autoChooser);
         
         // Schedule the selected autonomous command when the robot enters autonomous mode
         RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
     }
 
-    /**
-     * Defines the "Outpost" autonomous routine.
-     * This routine follows a sequence of trajectories defined in Choreo.
-     *
-     * @return The configured AutoRoutine.
-     */
-    private AutoRoutine OutpostRoutine() {
-        final AutoRoutine routine = autoFactory.newRoutine("Outpost");
-        
-        // Load trajectories from the generated ChoreoTraj constants
-        final AutoTrajectory startToShoot = OutpostTrajectory$0.asAutoTraj(routine);
-        final AutoTrajectory shootAndMovetoPreintake = OutpostTrajectory$1.asAutoTraj(routine);
-        final AutoTrajectory intakeThenMoveToShoot = OutpostTrajectory$2.asAutoTraj(routine);
-
-        // Define the sequence of events
-        routine.active().onTrue(
-            Commands.sequence(
-                startToShoot.resetOdometry(), // Reset pose to start of path
-                startToShoot.cmd()
-            )
-        );
-
-        // Keep Limelights idle while driving the first path. Useful for paths that rotate or move fast.
-        startToShoot.active().whileTrue(shooterLimelight.idle()); 
-        
-        // Chain the trajectories: when one finishes, start the next
-        startToShoot.done().onTrue(shootAndMovetoPreintake.cmd());
-        shootAndMovetoPreintake.done().onTrue(intakeThenMoveToShoot.cmd());
-
-        return routine;
-    }
-
-    private AutoRoutine resetPoseAtTrench() {
-        final AutoRoutine routine = autoFactory.newRoutine("reset Pose at trench");
-        final AutoTrajectory posTrajectory = ResetPoseOutpost.asAutoTraj(routine);
-
-        routine.active().onTrue(
-            posTrajectory.resetOdometry()
-        );
-
-        return routine;
-    }  
-    
     private AutoRoutine drivetoPreloadShootPos() {
         final AutoRoutine routine = autoFactory.newRoutine("Drive to preload shot");
         final AutoTrajectory posTrajectory = ShootPreloaded.asAutoTraj(routine);
@@ -144,7 +110,63 @@ public final class AutoRoutines {
         );
 
         posTrajectory.done().onTrue(new AimAndShoot(swerve, cowl, flywheel, feeder, floor, intakeRollers).withTimeout(5));
-
         return routine;
     }
+
+    private AutoRoutine leftBumpBeelineToCenter() {
+        final AutoRoutine routine = autoFactory.newRoutine("LEFT SIDE BUMP beeline to center");
+        final AutoTrajectory goOverBumpTraj = LeftCenterBeelineNoSweep$0.asAutoTraj(routine);
+        final AutoTrajectory getBallsInCenterTraj = LeftCenterBeelineNoSweep$1.asAutoTraj(routine);
+        final AutoTrajectory returnToShoot = LeftCenterBeelineNoSweep$2.asAutoTraj(routine);
+
+        routine.active().onTrue(
+            Commands.sequence(
+                goOverBumpTraj.resetOdometry(),
+                goOverBumpTraj.cmd()
+            )
+        );
+
+        goOverBumpTraj.done().onTrue(
+            intakePivot.deployCommand().withTimeout(Settings.IntakePivotSettings.INTAKE_DEPLOY_TIMEOUT)
+            .alongWith(getBallsInCenterTraj.cmd())
+            );
+
+        getBallsInCenterTraj.active().onTrue(intakeRollers.intakeCommand());
+        getBallsInCenterTraj.done().onTrue(returnToShoot.cmd());
+        returnToShoot.done().onTrue(new AimAndShoot(swerve, cowl, flywheel, feeder, floor, intakeRollers));
+
+        return routine;
+
+    }
+
+    private AutoRoutine leftBumpBeelineAndSweepCenter() {
+        final AutoRoutine routine = autoFactory.newRoutine("LEFT SIDE BUMP beeline and sweep center");
+        final AutoTrajectory goOverBumpTraj = LeftCenterBeelineSweep$0.asAutoTraj(routine);
+        final AutoTrajectory sweepTraj = LeftCenterBeelineSweep$1.asAutoTraj(routine);
+        final AutoTrajectory returnToShoot = LeftCenterBeelineSweep$2.asAutoTraj(routine);
+
+        routine.active().onTrue(
+            Commands.sequence(
+                goOverBumpTraj.resetOdometry(),
+                goOverBumpTraj.cmd()
+            )
+        );
+        
+        goOverBumpTraj.done().onTrue(
+            intakePivot.deployCommand().withTimeout(Settings.IntakePivotSettings.INTAKE_DEPLOY_TIMEOUT)
+            .alongWith(sweepTraj.cmd())
+            );
+
+        sweepTraj.active().whileTrue(intakeRollers.intakeCommand());
+        sweepTraj.done().onTrue(returnToShoot.cmd());
+
+        returnToShoot.done().onTrue(new AimAndShoot(swerve, cowl, flywheel, feeder, floor, intakeRollers));
+
+        return routine;
+
+    }
+
+
+
+
 }
